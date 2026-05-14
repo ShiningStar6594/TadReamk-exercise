fb9c74d2426a327e1e344309c6e1f2ed8cda7afb
ShiningStar6594 <130642703+ShiningStar6594@users.noreply.github.com>
2026-05-14 20:40:00 +0800

    Update whatsapp_send.py

--- whatsapp_send.py
+++ whatsapp_send.py
@@ -11,22 +11,22 @@ def whatsapp_sending(pdf, phone):
         
         page.goto("https://web.whatsapp.com")
         page.wait_for_load_state("networkidle")
-        time.sleep(15) 
+        time.sleep(10) 
 
         page.get_by_role("textbox").first.click()
         page.get_by_role("textbox").first.type(phone)
-        time.sleep(3)
+        time.sleep(1)
         page.keyboard.press("Enter")
-        time.sleep(3)
+        time.sleep(1)
         page.get_by_role("button", name="附加").click() 
-        time.sleep(3)
+        time.sleep(1)
         with page.expect_file_chooser() as fc_info:
             page.get_by_role("menuitem", name="文件").click()
             file_chooser = fc_info.value
             file_chooser.set_files(pdf)
-        time.sleep(5)
+        time.sleep(1)
         page.keyboard.press("Enter")
-        time.sleep(60)
+        time.sleep(40)
 
         context.close()
 
